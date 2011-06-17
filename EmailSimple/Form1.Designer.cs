9033f4d6f0acad09df258887b502e94bd59edf3c
ghy <ghy@you-may.cn>
2011-06-17 11:14:43 +0800

    完成接收email

---
+++ EmailSimple/Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace EmailSimple
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtServer = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnReceive = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(576, 268);
+            this.listBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(66, 294);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Server:";
+            // 
+            // txtServer
+            // 
+            this.txtServer.Location = new System.Drawing.Point(123, 291);
+            this.txtServer.Name = "txtServer";
+            this.txtServer.Size = new System.Drawing.Size(168, 21);
+            this.txtServer.TabIndex = 2;
+            this.txtServer.Text = "pop.qq.com";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(54, 321);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "UserName:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(123, 318);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(168, 21);
+            this.txtUserName.TabIndex = 2;
+            this.txtUserName.Text = "ghy@you-may.cn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Password:";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(123, 345);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(168, 21);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // btnReceive
+            // 
+            this.btnReceive.Location = new System.Drawing.Point(357, 308);
+            this.btnReceive.Name = "btnReceive";
+            this.btnReceive.Size = new System.Drawing.Size(75, 39);
+            this.btnReceive.TabIndex = 3;
+            this.btnReceive.Text = "Receive Email";
+            this.btnReceive.UseVisualStyleBackColor = true;
+            this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 385);
+            this.Controls.Add(this.btnReceive);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnReceive;
+    }
+}
+
---
+++ EmailSimple/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using ActiveUp.Net.Mail;
+
+
+
+
+namespace EmailSimple
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+        }
+
+        public void AddLogEntry(string entry)
+        {
+            DateTime d = DateTime.Now;
+            StringBuilder sb = new StringBuilder();
+            sb.Append(d.Hour.ToString().PadLeft(2, '0'));
+            sb.Append(":");
+            sb.Append(d.Minute.ToString().PadLeft(2, '0'));
+            sb.Append(":");
+            sb.Append(d.Second.ToString().PadLeft(2, '0'));
+            sb.Append(".");
+            sb.Append(d.Millisecond.ToString().PadLeft(3, '0'));
+            sb.Append(" | ");
+            sb.Append(entry);
+            this.listBox1.Items.Insert(0, sb.ToString());
+        }
+
+        private void btnReceive_Click(object sender, EventArgs e)
+        {
+            // We instantiate the pop3 client.
+            Pop3Client pop = new Pop3Client();
+
+            string server = txtServer.Text;
+            string username = txtUserName.Text;
+            string password = txtPassword.Text;
+
+            try
+            {
+                this.AddLogEntry(string.Format("Connection to the pop 3 server : {0}", server));
+
+                // Connect to the pop3 client
+                pop.Connect(server, username, password);
+
+                int count = pop.MessageCount;
+
+                this.AddLogEntry("Retrieve message list");
+
+                MessageCollection mc = new MessageCollection();
+
+                ActiveUp.Net.Mail.Message newMessage = pop.RetrieveMessageObject(count);
+                mc.Add(newMessage);
+                this.AddLogEntry(string.Format("Message ({0}) : {1}", count.ToString(), newMessage.Subject));
+
+                //for (int n = 1; n < pop.MessageCount + 1; n++)
+                //{
+                //    pop.RetrieveMessageObject(n);
+                //    mc.Add(newMessage);
+                //    this.AddLogEntry(string.Format("Message ({0}) : {1}", n.ToString(), newMessage.Subject));
+                //}
+
+            }
+
+            catch (Pop3Exception pexp)
+            {
+                this.AddLogEntry(string.Format("Pop3 Error: {0}", pexp.Message));
+            }
+            catch (Exception ex)
+            {
+                this.AddLogEntry(string.Format("Failed: {0}", ex.Message));
+            }
+
+            finally
+            {
+                if (pop.IsConnected)
+                {
+                    pop.Disconnect();
+                }
+            }
+        }
+    }
+}
