2aa7fd6002400b76d1980718c7dfb1c284c069ea
ghy <ghy@you-may.cn>
2011-10-10 12:52:28 +0800

    update

--- EmailSimple/Form1.Designer.cs
+++ EmailSimple/Form1.Designer.cs
@@ -30,12 +30,25 @@
         {
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.txtServer = new System.Windows.Forms.TextBox();
+            this.txtPopPort = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.txtPassword = new System.Windows.Forms.TextBox();
             this.btnReceive = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtPopServer = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSmtpPort = new System.Windows.Forms.TextBox();
+            this.txtSmtpServer = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // listBox1
@@ -45,47 +58,47 @@
             this.listBox1.ItemHeight = 12;
             this.listBox1.Location = new System.Drawing.Point(0, 0);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(576, 268);
+            this.listBox1.Size = new System.Drawing.Size(683, 268);
             this.listBox1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(66, 294);
+            this.label1.Location = new System.Drawing.Point(70, 319);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.Size = new System.Drawing.Size(59, 12);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Server:";
+            this.label1.Text = "Pop Port:";
             // 
-            // txtServer
+            // txtPopPort
             // 
-            this.txtServer.Location = new System.Drawing.Point(123, 291);
-            this.txtServer.Name = "txtServer";
-            this.txtServer.Size = new System.Drawing.Size(168, 21);
-            this.txtServer.TabIndex = 2;
-            this.txtServer.Text = "pop.126.com";
+            this.txtPopPort.Location = new System.Drawing.Point(134, 316);
+            this.txtPopPort.Name = "txtPopPort";
+            this.txtPopPort.Size = new System.Drawing.Size(168, 21);
+            this.txtPopPort.TabIndex = 2;
+            this.txtPopPort.Text = "110";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(54, 321);
+            this.label2.Location = new System.Drawing.Point(35, 393);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 12);
+            this.label2.Size = new System.Drawing.Size(89, 12);
             this.label2.TabIndex = 1;
-            this.label2.Text = "UserName:";
+            this.label2.Text = "Email Address:";
             // 
-            // txtUserName
+            // txtEmail
             // 
-            this.txtUserName.Location = new System.Drawing.Point(123, 318);
-            this.txtUserName.Name = "txtUserName";
-            this.txtUserName.Size = new System.Drawing.Size(168, 21);
-            this.txtUserName.TabIndex = 2;
-            this.txtUserName.Text = "youmaytest@126.com";
+            this.txtEmail.Location = new System.Drawing.Point(134, 390);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(168, 21);
+            this.txtEmail.TabIndex = 2;
+            this.txtEmail.Text = "FCSS@homs-fujitsu.com";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(54, 348);
+            this.label3.Location = new System.Drawing.Point(65, 420);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(59, 12);
             this.label3.TabIndex = 1;
@@ -93,16 +106,16 @@
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(123, 345);
+            this.txtPassword.Location = new System.Drawing.Point(134, 417);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
             this.txtPassword.Size = new System.Drawing.Size(168, 21);
             this.txtPassword.TabIndex = 2;
-            this.txtPassword.Text = "youmaytest00";
+            this.txtPassword.Text = "Fujitsu#2011";
             // 
             // btnReceive
             // 
-            this.btnReceive.Location = new System.Drawing.Point(357, 308);
+            this.btnReceive.Location = new System.Drawing.Point(134, 496);
             this.btnReceive.Name = "btnReceive";
             this.btnReceive.Size = new System.Drawing.Size(75, 39);
             this.btnReceive.TabIndex = 3;
@@ -110,17 +123,144 @@
             this.btnReceive.UseVisualStyleBackColor = true;
             this.btnReceive.Click += new System.EventHandler(this.btnReceive_Click);
             // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(454, 510);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 25);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 290);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Pop Server:";
+            // 
+            // txtPopServer
+            // 
+            this.txtPopServer.Location = new System.Drawing.Point(134, 287);
+            this.txtPopServer.Name = "txtPopServer";
+            this.txtPopServer.Size = new System.Drawing.Size(168, 21);
+            this.txtPopServer.TabIndex = 2;
+            this.txtPopServer.Text = "Mail.homs-fujitsu.com";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(390, 316);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Smtp Port:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(377, 287);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 12);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Smtp Server:";
+            // 
+            // txtSmtpPort
+            // 
+            this.txtSmtpPort.Location = new System.Drawing.Point(454, 313);
+            this.txtSmtpPort.Name = "txtSmtpPort";
+            this.txtSmtpPort.Size = new System.Drawing.Size(168, 21);
+            this.txtSmtpPort.TabIndex = 2;
+            this.txtSmtpPort.Text = "25";
+            // 
+            // txtSmtpServer
+            // 
+            this.txtSmtpServer.Location = new System.Drawing.Point(454, 284);
+            this.txtSmtpServer.Name = "txtSmtpServer";
+            this.txtSmtpServer.Size = new System.Drawing.Size(168, 21);
+            this.txtSmtpServer.TabIndex = 2;
+            this.txtSmtpServer.Text = "Mail.homs-fujitsu.com";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(425, 350);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(23, 12);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "To:";
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(454, 347);
+            this.txtTo.Multiline = true;
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(168, 58);
+            this.txtTo.TabIndex = 2;
+            this.txtTo.Text = "jonlywen@qq.com;jojin@you-may.cn;lily@pccw.net;ghy@you-may.cn";
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(454, 411);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(168, 21);
+            this.txtSubject.TabIndex = 2;
+            this.txtSubject.Text = "测试邮件";
+            // 
+            // txtBody
+            // 
+            this.txtBody.Location = new System.Drawing.Point(454, 438);
+            this.txtBody.Multiline = true;
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(168, 63);
+            this.txtBody.TabIndex = 2;
+            this.txtBody.Text = "测试的内容";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(395, 411);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Subject:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(413, 441);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(35, 12);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "Body:";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(576, 385);
+            this.ClientSize = new System.Drawing.Size(683, 552);
+            this.Controls.Add(this.btnSend);
             this.Controls.Add(this.btnReceive);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.txtEmail);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.txtServer);
+            this.Controls.Add(this.txtSmtpServer);
+            this.Controls.Add(this.txtPopServer);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtSmtpPort);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.txtPopPort);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Name = "Form1";
@@ -135,12 +275,25 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox txtServer;
+        private System.Windows.Forms.TextBox txtPopPort;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtEmail;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox txtPassword;
         private System.Windows.Forms.Button btnReceive;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtPopServer;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSmtpPort;
+        private System.Windows.Forms.TextBox txtSmtpServer;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
     }
 }
 
--- EmailSimple/Form1.cs
+++ EmailSimple/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using ActiveUp.Net.Mail;
+using System.Net.Mail;
+using System.IO;
 
 
 
@@ -47,17 +49,24 @@ namespace EmailSimple
         {
             this.listBox1.Items.Clear();
             // We instantiate the pop3 client.
-            Pop3Client pop = new Pop3Client();
+            Pop3Client pop3Client = new Pop3Client();
 
-            string server = txtServer.Text;
-            string username = txtUserName.Text;
+            string server = this.txtPopServer.Text;
+            string username = txtEmail.Text;
             string password = txtPassword.Text;
+            int popPort = Convert.ToInt32(txtPopPort.Text);
 
             try
             {
-                pop.Connect(server, username, password);
 
-                int count = pop.MessageCount;
+                // pop3Client.APOPConnect(server, popPort, username, password);
+
+                pop3Client.Connect(server, popPort, username, password);
+                //   pop3Client.Authenticate(username, password, SaslMechanism.CramMd5);
+
+
+                int count = pop3Client.MessageCount;
+
 
                 this.AddLogEntry(string.Format("共收到{0}封 ：", count));
 
@@ -67,10 +76,38 @@ namespace EmailSimple
                 //mc.Add(newMessage);
                 // this.AddLogEntry(string.Format("Message ({0}) : {1}", count.ToString(), newMessage.Subject));
 
-                for (int n = count; n >= 1; n--)
+                //for (int n = count; n >= 1; n--)
+                //{
+                //    var msg = pop.RetrieveMessageObject(n);
+                //    this.AddLogEntry(string.Format("{0}-({1}) : {2}", n.ToString(), msg.Date.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss"), msg.Subject));
+                //}
+
+
+                string str = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+
+                DateTime d;
+                DateTime.TryParse(str, out d);
+
+
+                var date = DateTime.Now.AddDays(-6);
+
+
+                while (count > 0)
                 {
-                    var msg = pop.RetrieveMessageObject(n);
-                    this.AddLogEntry(string.Format("{0}-({1}) : {2}", n.ToString(), msg.Date.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss"), msg.Subject));
+                    var msg = pop3Client.RetrieveMessageObject(count);
+
+                    if (msg.Date.ToLocalTime() < date)
+                        break;
+                    var subject = msg.Subject;
+
+                    if (subject.Contains("问题回复-【") && subject.Contains("【") && subject.Contains("】"))
+                    {
+                        var index = subject.IndexOf("【") + 1;
+                        var issueNo = subject.Substring(index, subject.IndexOf("】") - index);
+                    }
+
+                    this.AddLogEntry(string.Format("{0}-({1}) : {2}", count.ToString(), msg.Date.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss"), msg.Subject));
+                    count--;
                 }
 
             }
@@ -85,11 +122,105 @@ namespace EmailSimple
 
             finally
             {
-                if (pop.IsConnected)
+                if (pop3Client.IsConnected)
                 {
-                    pop.Disconnect();
+                    pop3Client.Disconnect();
                 }
             }
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            sendmail();
+        }
+
+        private void AuSendmail()
+        {
+
+        }
+
+        private void sendmail()
+        {
+            string smtpServer = this.txtSmtpServer.Text;
+            string email = txtEmail.Text;
+            string password = txtPassword.Text;
+            int smtpPort = Convert.ToInt32(txtSmtpPort.Text);
+            string to = txtTo.Text;
+
+            MailMessage message = new MailMessage();
+            message.SubjectEncoding = Encoding.UTF8; //邮件编码 
+            var from = new MailAddress(email, "Hello");//你的邮
+            message.From = from;
+
+            foreach (var item in to.Trim(';').Split(';'))
+            {
+                message.To.Add(new MailAddress(item));
+            }
+
+            message.Subject = txtSubject.Text;
+            //   message.CC.Add(new MailAddress("3400415@qq.com")); //抄送
+            message.IsBodyHtml = true; //正文是否是HTML
+            message.BodyEncoding = System.Text.Encoding.UTF8;
+            message.Body = txtBody.Text; //正文
+            //FileStream fs = File.OpenRead(@"z:\FCSS\Upload\90979535-9877-4106-926a-e63458528be8.jpg");
+
+            //message.Attachments.Add(new System.Net.Mail.Attachment(fs, "aaa.txt")); //附件
+
+            message.Priority = System.Net.Mail.MailPriority.High; //优先级
+            System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient(smtpServer, smtpPort); // 587;//Gmail使用的端口
+            client.Credentials = new System.Net.NetworkCredential(email, password); //这里是申请的邮箱和密码
+            client.EnableSsl = false;//必须经过ssl加密
+
+            try
+            {
+                client.Send(message);
+                MessageBox.Show("邮件已经成功发送到" + message.To.ToString());
+            }
+            catch (Exception ee)
+            {
+                MessageBox.Show(ee.Message);
+            }
+        }
+
+
+        //private void sendmail()
+        //{
+        //    string smtpServer = this.txtSmtpServer.Text;
+        //    string email = txtEmail.Text;
+        //    string password = txtPassword.Text;
+        //    int smtpPort = Convert.ToInt32(txtSmtpPort.Text);
+        //    string to = txtTo.Text;
+
+        //    MailMessage message = new MailMessage();
+        //    message.SubjectEncoding = Encoding.UTF8; //邮件编码 
+        //    var from = new MailAddress(email, "Hello");//你的邮
+        //    message.From = from;
+
+        //    foreach (var item in to.Trim(';').Split(';'))
+        //    {
+        //        message.To.Add(new MailAddress(item));
+        //    }
+
+        //    message.Subject = txtSubject.Text;
+        //    //   message.CC.Add(new MailAddress("3400415@qq.com")); //抄送
+        //    message.IsBodyHtml = true; //正文是否是HTML
+        //    message.BodyEncoding = System.Text.Encoding.UTF8;
+        //    message.Body = txtBody.Text; //正文
+        //    //message.Attachments.Add(new Attachment(@"c:\1.txt")); //附件
+        //    message.Priority = System.Net.Mail.MailPriority.High; //优先级
+        //    System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient(smtpServer, smtpPort); // 587;//Gmail使用的端口
+        //    client.Credentials = new System.Net.NetworkCredential(email, password); //这里是申请的邮箱和密码
+        //    client.EnableSsl = true;//必须经过ssl加密
+
+        //    try
+        //    {
+        //        client.Send(message);
+        //        MessageBox.Show("邮件已经成功发送到" + message.To.ToString());
+        //    }
+        //    catch (Exception ee)
+        //    {
+        //        MessageBox.Show(ee.Message);
+        //    }
+        //}
     }
 }
